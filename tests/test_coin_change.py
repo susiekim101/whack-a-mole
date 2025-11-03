99e532cfe3835b6939d3f961ee7d6524ebac18f9
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-03 09:31:41 -0800

    add non greedy failing test case

--- tests/test_coin_change.py
+++ tests/test_coin_change.py
@@ -5,3 +5,8 @@ def test_coin_change_example():
     coins = [1, 2, 5]
     amount = 11
     assert coin_change(coins, amount) == 3
+
+def test_non_greedy():
+    coins = [5, 3]
+    amount = 14
+    assert coin_change(coins, amount) == 14
